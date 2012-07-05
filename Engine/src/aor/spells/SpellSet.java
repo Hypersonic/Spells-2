4c755c6ab7c4dd3dc9ff74382ea6f30297d49f7c
jayhf <jayhf614@aol.com>
2012-07-05 08:18:10 -0400

    I added support for loading spells from .jar files, after a lot of tedious testing. I also added spellsets which allow people to package multiple spells into a .jar file or .class file by extending SpellSet instead of Spell and returning an arraylist of spells to be used.

---
+++ Engine/src/aor/spells/SpellSet.java
@@ -0,0 +1,7 @@
+package aor.spells;
+
+import java.util.ArrayList;
+
+public abstract class SpellSet {
+	public abstract ArrayList<Spell> getSpells();
+}
--- Engine/src/aor/spells/Spells.java
+++ Engine/src/aor/spells/Spells.java
@@ -1,6 +1,8 @@
 package aor.spells;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.logging.Logger;
@@ -16,21 +18,62 @@ public class Spells extends JavaPlugin{
 	}
 	public void onEnable() {
 		loadSpells();
+		if(spells.size()==0){
+			log.warning("No Spells Loaded!");
+			onDisable();
+			return;
+		}
 		for(Spell spell:spells){
 			log.info(spell.getName());
 		}
 		log.info("Spells 2.0 Enabled");
 	}
 	private void loadSpells(){
+		File spelldir=new File("plugins/spells/");
+		if(!spelldir.exists()){
+			if(spelldir.canWrite()){
+				try {
+					spelldir.createNewFile();
+				} catch (IOException e) {}
+			}
+			else{
+				//TODO write this
+			}
+		}
+		else{
+			//TODO write this
+		}
+		PluginClassLoader loader=(PluginClassLoader) Spells.class.getClassLoader();
+		try{
+			loader.addURL(new File("plugins/spells/").toURI().toURL());
+		} catch(Throwable t){}
 		for(File f:Arrays.asList(new File("plugins/spells/").listFiles())){
 			if(f.getName().endsWith(".class")){
 				try {
-					PluginClassLoader loader=(PluginClassLoader) Spells.class.getClassLoader();
-					loader.addURL(new File("plugins/spells/").toURI().toURL());
 					Class<?> clazz = loader.loadClass(f.getName().replace(".class", ""));
-					Class<? extends Spell> s=clazz.asSubclass(Spell.class);
-					spells.add(s.newInstance());
-				} catch (Throwable t) {
+					if(Spell.class.isInstance(clazz.newInstance())){
+						Class<? extends Spell> s=clazz.asSubclass(Spell.class);
+						spells.add(s.newInstance());
+					}
+					else if(SpellSet.class.isInstance(clazz.newInstance())){
+						Class<? extends SpellSet> s=clazz.asSubclass(SpellSet.class);
+						spells.addAll(s.newInstance().getSpells());
+					}
+				} catch (Throwable t) {}
+			}
+			else if(f.getName().endsWith(".jar")){
+				try{
+					loader.addURL(new URL(new URL("jar:"+f.toURI().toURL()+"!/"),""));
+					Class<?> clazz = loader.loadClass(f.getName().replace(".jar", ""));
+					if(Spell.class.isInstance(clazz.newInstance())){
+						Class<? extends Spell> s=clazz.asSubclass(Spell.class);
+						spells.add(s.newInstance());
+					}
+					else if(SpellSet.class.isInstance(clazz.newInstance())){
+						Class<? extends SpellSet> s=clazz.asSubclass(SpellSet.class);
+						spells.addAll(s.newInstance().getSpells());
+					}
+				} catch(Throwable t){
 					t.printStackTrace();
 				}
 			}
