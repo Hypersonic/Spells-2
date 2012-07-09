2f67e4532cda17eb89c4aac3d95cf9db8d897bc9
jayhf <jayhf614@aol.com>
2012-07-08 22:34:11 -0400

    I continued work on the api and added cast() and checkRequirements(player) in Spell

--- Engine/src/aor/spells/Spell.java
+++ Engine/src/aor/spells/Spell.java
@@ -1,6 +1,10 @@
 package aor.spells;
 
+import org.bukkit.entity.Player;
+
 public abstract class Spell {
 	public abstract String getName();
 	public abstract String getDescription();	
+	public abstract void cast();
+	public abstract boolean checkRequirements(Player player);
 }
