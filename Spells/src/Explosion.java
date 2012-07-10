2606fff6ab2c6c5dfe49e2ff43dce1be008ffe14
jayhf <jayhf614@aol.com>
2012-07-10 13:09:46 -0400

    made explosion check it's range requirement in checkRequirement, instead of cast, so listeners will know it was cancelled

--- Spells/src/Explosion.java
+++ Spells/src/Explosion.java
@@ -9,41 +9,41 @@ import org.bukkit.Material;
  */
 public class Explosion extends Spell {
 
-    private static final int MAXDISTANCE = 200; // Sets the maximum distance.
+	private static final int MAXDISTANCE = 200; // Sets the maximum distance.
 
-    public Explosion(){
-        
-    }
-
-    @Override
-    public String getName() {
-        return "Explosion"; 
-    }
-    
-    @Override
-    public String getDescription() {
-        return "Creates an explosion at the block the player is looking at";
-    }
-
-    @Override
-    public void cast(Player player) {
-        Block targetBlock = player.getTargetBlock(null, MAXDISTANCE);
-        if (targetBlock.getType() != Material.AIR) {
-            int explosionSize = 5;
-            if (player.getLevel() > 100) {
-                explosionSize = 30;
-                player.sendMessage("You feel the great arcane energies of your ancestors build up inside of you...");
-            } else if (player.getLevel() > 30) {
-                explosionSize = 10;
-                player.sendMessage("You feel a power building within you, ready to explode, and focus on your target...");
-            } else {
-                player.sendMessage("You feel a power awaken within you, and you prepare to strike...");
-            }
-
-            targetBlock.getWorld().createExplosion(targetBlock.getLocation().getX(), targetBlock.getLocation().getY(), targetBlock.getLocation().getZ(), explosionSize, false);
-
-        } else {
-            player.sendMessage("Could not cast, you're not pointing at anything!");
-        }
-    }
+	public Explosion(){
+		
+	}
+	@Override
+	public String getName() {
+		return "Explosion"; 
+	}
+	
+	@Override
+	public String getDescription() {
+		return "Creates an explosion at the block the player is looking at";
+	}
+	@Override
+	public boolean checkRequirements(Player player){
+		if(player.getTargetBlock(null, MAXDISTANCE).getType().equals(Material.AIR)){
+			player.sendMessage("Could not cast, you're not pointing at anything!");
+			return false;
+		}
+		else return true;
+	}
+	@Override
+	public void cast(Player player) {
+		final Block targetBlock=player.getTargetBlock(null, MAXDISTANCE);
+		int explosionSize = 5;
+		if (player.getLevel() > 100) {
+			explosionSize = 30;
+			player.sendMessage("You feel the great arcane energies of your ancestors build up inside of you...");
+		} else if (player.getLevel() > 30) {
+			explosionSize = 10;
+			player.sendMessage("You feel a power building within you, ready to explode, and focus on your target...");
+		} else {
+			player.sendMessage("You feel a power awaken within you, and you prepare to strike...");
+		}
+		targetBlock.getWorld().createExplosion(targetBlock.getLocation().getX(), targetBlock.getLocation().getY(), targetBlock.getLocation().getZ(), explosionSize, false);
+	}
 }
