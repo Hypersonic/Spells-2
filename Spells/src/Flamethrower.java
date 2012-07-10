6464ad25ff6d488f9b6d6a5fc34ed8ba445b4b10
Josh Hofing <joshhofing@gmail.com>
2012-07-10 17:16:48 -0400

    Started the Flamethrower Spell.
        It needs the scheduler to be finished, but that's all.
        It's fun as is, too! ^_^

---
+++ Spells/src/Flamethrower.java
@@ -0,0 +1,43 @@
+import aor.spells.Spell;
+
+import org.bukkit.entity.Player;
+import org.bukkit.block.Block;
+import org.bukkit.Material;
+
+import java.util.List;
+
+/**
+ * Creates a line of fire in the direction that the player is looking at for a few seconds
+ */
+public class Flamethrower extends Spell {
+
+	private static final int MAXDISTANCE = 12; // Sets the maximum distance.
+
+	public Flamethrower(){
+		
+	}
+	@Override
+	public String getName() {
+		return "Flamethrower"; 
+	}
+	
+	@Override
+	public String getDescription() {
+		return "Creates a line of fire in the direction that the player is looking at, for a few seconds";
+	}
+	@Override
+	public boolean checkRequirements(Player player){
+		return true;
+	}
+	@Override
+	public void cast(Player player) {
+	
+        List<Block> seenBlocks = player.getLineOfSight(null, MAXDISTANCE);
+        for (Block block : seenBlocks) {
+            if (block.getType() == Material.AIR) {
+                block.setType(Material.FIRE);
+            }
+        }
+
+    }
+}
