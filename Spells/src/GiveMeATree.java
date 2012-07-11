0014f936dfa04f0185094310ee0b3936c3d80143
Justin Yulli Kim <yulli@yulli.org>
2012-07-11 18:53:29 -0400

    Now we can make trees wherever

---
+++ Spells/src/GiveMeATree.java
@@ -0,0 +1,41 @@
+import aor.spells.Spell;
+
+import org.bukkit.entity.Player;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.TreeType;
+import org.bukkit.Material;
+import org.bukkit.World;
+
+/**
+ * Grows a tree where the player is pointing
+ */
+public class GiveMeATree extends Spell {
+
+	private static final int MAXDISTANCE = 10;
+
+	public GiveMeATree(){
+		
+	}
+	@Override
+	public String getName() {
+		return "GiveMeATree"; 
+	}
+	
+	@Override
+	public String getDescription() {
+		return "Grows a tree where the player is pointing";
+	}
+	@Override
+	public boolean checkRequirements(Player player){
+		return true;
+	}
+	@Override
+	public void cast(Player player) {
+        final Block target = player.getTargetBlock(null, MAXDISTANCE);
+        final Block above = target.getRelative(BlockFace.UP);
+        if (target.getType().equals(Material.DIRT) && above.getType().equals(Material.AIR)) {
+            target.getWorld().generateTree(above.getLocation(), TreeType.TREE);
+        }
+    }
+}
