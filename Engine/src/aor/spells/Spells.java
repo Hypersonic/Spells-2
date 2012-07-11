083bcc5961946d72904f5d0fea69b43ea02d8da9
jayhf <jayhf614@aol.com>
2012-07-11 18:21:26 -0300

    I added the imports I forgot to add for the new events, making Spells compile again.

--- Engine/src/aor/spells/Spells.java
+++ Engine/src/aor/spells/Spells.java
@@ -16,7 +16,9 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.PluginClassLoader;
 
