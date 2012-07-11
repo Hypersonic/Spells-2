e62b95cdfbc9d4f4b1dc9d4438a40c5131ec2dc1
Josh Hofing <joshhofing@gmail.com>
2012-07-11 16:17:12 -0400

    Scheduling added to flamethrower.
        Not fully functional due to a bug in scheduler.

--- Spells/src/Flamethrower.java
+++ Spells/src/Flamethrower.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.block.Block;
 import org.bukkit.Material;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 /**
@@ -12,11 +13,14 @@ import java.util.List;
 public class Flamethrower extends Spell {
 
 	private static final int MAXDISTANCE = 12;
-
+    private Method ignite;
 	public Flamethrower(){
-		
+		try {
+			ignite=Flamethrower.class.getMethod("ignite", Player.class);
+		} catch (Exception e) {assert false:"Who broke it???";}
 	}
-	@Override
+	
+    @Override
 	public String getName() {
 		return "Flamethrower"; 
 	}
@@ -25,19 +29,25 @@ public class Flamethrower extends Spell {
 	public String getDescription() {
 		return "Creates a line of fire in the direction that the player is looking at, for a few seconds";
 	}
-	@Override
+	
+    @Override
 	public boolean checkRequirements(Player player){
 		return true;
 	}
-	@Override
-	public void cast(Player player) {
 	
+    @Override
+	public void cast(Player player) {
+	    for (int i = 0; i < 3000; i++) {
+            schedule(i, ignite, player);
+        }
+    }
+
+    public void ignite(Player player){ 
         List<Block> seenBlocks = player.getLineOfSight(null, MAXDISTANCE);
         for (Block block : seenBlocks) {
             if (block.getType() == Material.AIR) {
                 block.setType(Material.FIRE);
             }
         }
-
     }
 }
