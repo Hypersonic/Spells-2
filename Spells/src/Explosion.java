23fa2f4a374871a8604f20cd738e566f525d3639
Justin Yulli Kim <yulli@yulli.org>
2012-07-11 22:54:58 -0400

    Nerf Explosion a bit

--- Spells/src/Explosion.java
+++ Spells/src/Explosion.java
@@ -36,13 +36,13 @@ public class Explosion extends Spell {
 		final Block targetBlock=player.getTargetBlock(null, MAXDISTANCE);
 		int explosionSize;
 		if (player.getLevel() >= 100) {
-			explosionSize = 30;
+			explosionSize = 20;
 			player.sendMessage("You feel the great arcane energies of your ancestors build up inside of you...");
 		} else if (player.getLevel() >= 30) {
-			explosionSize = 10;
+			explosionSize = 7;
 			player.sendMessage("You feel a power building within you, ready to explode, and focus on your target...");
 		} else {
-			explosionSize = 5;
+			explosionSize = 3;
 			player.sendMessage("You feel a power awaken within you, and you prepare to strike...");
 		}
 		targetBlock.getWorld().createExplosion(targetBlock.getLocation().getX(), targetBlock.getLocation().getY(), targetBlock.getLocation().getZ(), explosionSize, false);
