7ed609aef73a20e12559abd048d929d54ad0c65d
Josh Hofing <joshhofing@gmail.com>
2012-07-20 08:13:28 +0300

    Tornado has random X,Y offsets.
        Moved height offset to a variable.
    
        Code is better commented.
    
        Removed old bits of commented out code.
    
        Commented out debug bits of code.

--- Spells/src/Tornado.java
+++ Spells/src/Tornado.java
@@ -34,7 +34,7 @@ public class Tornado extends Spell {
 
     public void cast(Player player) {
         
-        boolean castAgain = false;
+        boolean castAgain = false; // We only want to be casting it one additional time, so lets use a variable
         List<Entity> nearbyEntities = player.getNearbyEntities(10,50,10);
         for (Entity entity : nearbyEntities) {
             // Y is actually height in minecraft, so the value we'll be calling Y is really Z
@@ -43,25 +43,14 @@ public class Tornado extends Spell {
             double entityX = entity.getLocation().getX();
             double entityY = entity.getLocation().getZ();
             
-            // if we increase the x value we're using by the difference in the y value... maybe
+            // Relative X and Y values (based on the entity, not the player)
             double Ydiff = entityY - playerY;
             double Xdiff = entityX - playerX;
-        /*
-            double newX = Ydiff / DILUTEAMOUNT; // -1 * (Ydiff / 4);
-            double newY = Xdiff / DILUTEAMOUNT; // -1 * (Xdiff / 4);//newX * ((-1 / slope) / 10);
-
-            // and now create the vector and apply the force, hopefully.
-            Vector force = new Vector(newX, 0.5, newY);
-
-            //entity.setVelocity(entity.getVelocity().add(force));
-            entity.setVelocity(force);
-        */
-
-            // My attempt at actually using my trig knowledge:
-            double distance = player.getLocation().distance(entity.getLocation());
-            double theta = Math.acos(Xdiff / distance);
             
-            //theta -= Math.PI / 2; // Decrement by 90 degrees (or Pi/2 radians)
+            double distance = player.getLocation().distance(entity.getLocation());
+            double theta = Math.acos(Xdiff / distance); // Use the arc cosine of the X difference to get the relative theta
+            
+            // We don't want the theta to go over 2Pi, because then it starts acting funky. Yeah, I know.
             if (theta > (3 / 2) * Math.PI) {
                 theta -= Math.PI / 2;
             } else if (theta < Math.PI / 2) {
@@ -72,15 +61,20 @@ public class Tornado extends Spell {
 
             double rotatedX = Math.cos(theta) * RADIUS;
             double rotatedY = Math.sin(theta) * RADIUS;
+            
+            // Slight random offsets.
+            double Yoffset = (Math.random() - 0.5);
+            double Xoffset = (Math.random() - 0.5);
+            double HeightOffset = (Math.random());
 
-            Vector force = new Vector(rotatedX, Math.random(), rotatedY);
-            //entity.setVelocity(entity.getVelocity().add(force));
+            Vector force = new Vector(rotatedX + Xoffset, HeightOffset, rotatedY + Yoffset); // Our actual force that acts on the entity, uses a random offset
+            //entity.setVelocity(entity.getVelocity().add(force)); // apply the velocity
             entity.setVelocity(force);
 
-            entity.getLocation().getBlock().getRelative(0,-1,0).setType(Material.DIAMOND_BLOCK);
+            //entity.getLocation().getBlock().getRelative(0,-1,0).setType(Material.DIAMOND_BLOCK); //Trace the path with diamond blocks
             
-            player.sendMessage("Theta: " + theta);
-            player.sendMessage(force.toString());
+            //player.sendMessage("Theta: " + theta);
+            //player.sendMessage(force.toString());
             castAgain = true;
         }
         if (castAgain) {
